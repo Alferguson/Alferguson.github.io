875c036fe5ff9d6ab44a3f1081f49745a8632931
alferguson <alferguson@berkeley.edu>
2020-04-24 23:23:14 -0700

    Add cool text gradient to header text

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,19 +1,23 @@
 import React from "react";
 import styles from "./layout.module.css";
+import NavBar from "../navbar";
 
 // Wraps every page with header, navbar, and footer
 // Main contains actual page components
-const Layout = props => {
+const Layout = (props) => {
   const { children, location, title } = props;
 
   return (
-    <div className={styles.pageTemp}>
+    <div className={styles.layout}>
       <header>
         <h1>John Alexander Ferguson</h1>
         <hr></hr>
         {/* <Navbar location={location} title={title} /> */}
       </header>
-      <main>{children}</main>
+      <div>
+        <NavBar />
+        <main>{children}</main>
+      </div>
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
